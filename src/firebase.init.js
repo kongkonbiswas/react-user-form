e3374a688a6d5f0748f3ade5a3c28a76d4c553eb
Kongkon Biswas <kongkonbiswas3241@gmail.com>
2022-08-21 23:56:28 +0600

    authentication, form and so on added

---
+++ src/firebase.init.js
@@ -0,0 +1,21 @@
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC5Vq7kLLjd1Farzx5N2__iqY_AVmRPMEA",
+  authDomain: "react-user-form.firebaseapp.com",
+  projectId: "react-user-form",
+  storageBucket: "react-user-form.appspot.com",
+  messagingSenderId: "200983889405",
+  appId: "1:200983889405:web:fc6683cbc2829fa6b24594"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
